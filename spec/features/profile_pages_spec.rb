0a6e2afddc4f8f89e431ef7a7554e95d6d45d088
Dorrit <dorritgeshuri@gmail.com>
2013-04-10 16:18:18 -0700

    starts the profile functions

--- spec/features/profile_pages_spec.rb
+++ spec/features/profile_pages_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 feature "new profile" do
   scenario "with valid info" do
-    visit new_profile_path
+    sign_up_helper
+    click_button "Create Contact detail"
     fill_in 'Bio', :with => 'I am an astronaut'
     fill_in "Hobby", :with => 'Soccer, spulunking'
     fill_in "Work", :with => 'Code'
     click_button "Create Profile"
-    page.should have_content("Your profile was successfully created.")
+    page.should have_content "Your profile was successfully created."
   end
 
 
