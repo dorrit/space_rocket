bf32fc7d047e9a197a68ee2ea41493ad2d15c481
Epicodus <student@epicodus.com>
2013-04-10 16:50:48 -0700

    adds messages index, integration test, and fixes redirect

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,6 @@ class Message < ActiveRecord::Base
   validates :subject, :text, :presence => true
   attr_accessible :subject, :text, :public
   belongs_to :rocketeer
-  after_initialize :default_values
 
-  def default_values
-    self.public = false
-  end
 end
 
